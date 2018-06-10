b9545561e6cf93470f6ba65ebc22557cce931c1c
mram <mram@umd.edu>
2018-06-10 15:04:23 -0400

    Added sound support.

--- notifications.py
+++ notifications.py
@@ -1,6 +1,11 @@
-def notify(title='', subtitle='', message=''):
+def notify(title='', subtitle='', message='', sound=None):
     """
     Function used to send notification.
+
+    Valid sound names are names of sounds found in -
+    ~/Library/Sounds
+    /System/Library/Sounds
+
     Variable ordering is as follows -
 
     :param title: Notification Title. Default: Empty
@@ -10,7 +15,10 @@ def notify(title='', subtitle='', message=''):
     """
     from subprocess import Popen, PIPE
 
-    script = f'display notification "{message}" with title "{title}" subtitle "{subtitle}"'
-    print(script)
+    if not sound:
+        script = f'display notification "{message}" with title "{title}" subtitle "{subtitle}"'
+    else:
+        script = f'display notification "{message}" with title "{title}" subtitle "{subtitle}" sound name "{sound}"'
+
     p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     stdout, stderr = p.communicate(script)
